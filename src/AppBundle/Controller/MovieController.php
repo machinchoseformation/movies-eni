bc53adc5898215d8f39962ec0736e374f3a220f5
guillaume <machinchoseformation@gmail.com>
2017-09-26 13:10:33 +0200

    etape 1 liste des films

---
+++ src/AppBundle/Controller/MovieController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class MovieController extends Controller
+{
+    public function homeAction()
+    {
+        $repo = $this->getDoctrine()->getRepository("AppBundle:Movie");
+        $movies = $repo->findBy([], ["dateCreated" => "DESC"], 50, 0);
+
+        return $this->render('AppBundle:Movie:home.html.twig', [
+            "movies" => $movies
+        ]);
+    }
+
+}
